f135c9948092d5a9cf2d1ac2ee3b0ea61a165db5
stellack <chemutaistella01@gmail.com>
2023-03-04 15:42:02 +0300

    Added footer, secondary navbar component

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FooterComponent } from '../footer/footer.component';
+import { SecondaryNavbarComponent } from './secondary-navbar/secondary-navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    FooterComponent,
+    SecondaryNavbarComponent
   ],
   imports: [
     CommonModule
